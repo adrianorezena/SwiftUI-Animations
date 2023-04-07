0f916433dc46254decd12f785fa63f65a5c093de
adrianorezena <adrianocr@gmail.com>
2023-04-07 08:22:10 -0300

    Initial Commit

---
+++ SwiftUI Animations/SwiftUI_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationsApp.swift
+//  SwiftUI Animations
+//
+//  Created by Adriano Rezena on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
